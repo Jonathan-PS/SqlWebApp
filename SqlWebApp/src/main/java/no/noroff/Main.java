80168399c95641ee3554710644d0020b7b8b512e
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-02-25 13:33:40 +0100

    Added insert methods

--- SqlWebApp/src/main/java/no/noroff/Main.java
+++ SqlWebApp/src/main/java/no/noroff/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Jonathan was here!");
         System.out.println("Thomas was not here");
+
+        TheSqlConnection db = new TheSqlConnection();
+        db.connect();
     }
 }
