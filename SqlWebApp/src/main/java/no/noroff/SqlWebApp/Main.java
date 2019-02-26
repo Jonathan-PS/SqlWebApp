e9ad724867e3bc25bda82faf6d65cedc0c27dacc
JonathanPS <jonathanpstang@gmail.com>
2019-02-26 10:15:03 +0100

    Method: selectPerson() and Class: Person added

--- SqlWebApp/src/main/java/no/noroff/SqlWebApp/Main.java
+++ SqlWebApp/src/main/java/no/noroff/SqlWebApp/Main.java
@@ -41,5 +41,13 @@ public class Main {
         conn.insertRelationship(4,5,"Sister","Brother");
         conn.insertRelationship(8,10,"Father","Daughter");
 
+
+        // SELECT PERSON TESTING
+        Person person1 = conn.selectPerson(1);
+        Person person2 = conn.selectPerson(2);
+
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
     }
 }
