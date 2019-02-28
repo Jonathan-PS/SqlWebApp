f2014d999502efa0ea78192f5fafeba4df2b0292
Trudelutten <t.b.hjelmeland@gmail.com>
2019-02-28 14:20:21 +0100

    added more email and phone

--- SqlWebApp/src/main/java/no/noroff/SqlWebApp/controllers/SearchController.java
+++ SqlWebApp/src/main/java/no/noroff/SqlWebApp/controllers/SearchController.java
@@ -1,14 +1,27 @@
 package no.noroff.SqlWebApp.controllers;
 
+
 import no.noroff.SqlWebApp.SqlWebApplication;
 import no.noroff.SqlWebApp.UserInput;
+import no.noroff.SqlWebApp.models.Person;
 import no.noroff.SqlWebApp.models.Relationship;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Collections;
+import java.util.List;
+
+import java.util.ArrayList;
+
 @Controller
 public class SearchController {
+    UserInput userInput;
+
     private String templateName = "menuTemplate";
     @GetMapping("/menu")
     public String menu(Model model) {
@@ -16,11 +29,15 @@ public class SearchController {
         return templateName;
     }
 
-    @PostMapping("/menu") //use the shorthand
-    public String menuResult(@ModelAttribute("userInput") UserInput userInput) {
-           System.out.println("value: " + userInput.getValue());
-        System.out.println("dropdown: " + userInput.getAttribute());
-           return "menuTemplate";
 
-    }
+
+
+
+
+
+
+
+
+
+
 }
