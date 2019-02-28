89f3533efe15b37bd2c219d5497311bb2e222eba
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-02-28 14:17:31 +0100

    Added search controller input

--- SqlWebApp/src/main/java/no/noroff/SqlWebApp/controllers/PersonController.java
+++ SqlWebApp/src/main/java/no/noroff/SqlWebApp/controllers/PersonController.java
@@ -1,6 +1,7 @@
 package no.noroff.SqlWebApp.controllers;
 
 import no.noroff.SqlWebApp.SqlWebApplication;
+import no.noroff.SqlWebApp.UserInput;
 import no.noroff.SqlWebApp.models.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,22 @@ public class PersonController {
         ArrayList<Person> personList = SqlWebApplication.sqlConn.selectAllPersons();
 
         return personList;
+    }
+
+    @GetMapping("/menu/list")
+    public static ArrayList<Person> personSearch(UserInput userInput) {
+        ArrayList<Person> pl = new ArrayList<Person>();
+        if(userInput.getValue() != null && userInput.getAttribute() != null){
+            pl = SqlWebApplication.sqlConn.selectPersonByName(userInput.getAttribute(),userInput.getValue());
+            for (Person p : pl){
+                System.out.println(p.getpID() + " " + p.getFirstName() + " " + p.getLastName());
+            }
+            System.out.println(pl.get(0).getFirstName());
+        }
+        //ArrayList<Person> personList = SqlWebApplication.sqlConn.selectPersonByName(userInput.getAttribute(),userInput.getValue());
+
+        System.out.println("was called");
+        return pl;
 
     }
 
--- SqlWebApp/src/main/java/no/noroff/SqlWebApp/controllers/SearchController.java
+++ SqlWebApp/src/main/java/no/noroff/SqlWebApp/controllers/SearchController.java
@@ -2,14 +2,22 @@ package no.noroff.SqlWebApp.controllers;
 
 import no.noroff.SqlWebApp.SqlWebApplication;
 import no.noroff.SqlWebApp.UserInput;
+import no.noroff.SqlWebApp.models.Person;
 import no.noroff.SqlWebApp.models.Relationship;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+
 
 @Controller
 public class SearchController {
     private String templateName = "menuTemplate";
+    UserInput userInput;
+
     @GetMapping("/menu")
     public String menu(Model model) {
         model.addAttribute("userInput", new UserInput());
@@ -18,9 +26,17 @@ public class SearchController {
 
     @PostMapping("/menu") //use the shorthand
     public String menuResult(@ModelAttribute("userInput") UserInput userInput) {
-           System.out.println("value: " + userInput.getValue());
-        System.out.println("dropdown: " + userInput.getAttribute());
-           return "menuTemplate";
-
+        PersonController pc = new PersonController();
+        this.userInput = userInput;
+        pc.personSearch(this.userInput);
+        return "redirect:/menu/list";
     }
+
+    /*@GetMapping("/menu/output") //use the shorthand
+    public ArrayList<Person> menuResult() {
+        ArrayList<Person> personList = new ArrayList<Person>();
+        //personList = SqlWebApplication.sqlConn.selectPersonByName(userInput.getAttribute(), userInput.getValue());
+        return personList;
+    }*/
+
 }
