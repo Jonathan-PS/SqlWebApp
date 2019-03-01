30c03d6b74d33ee4dda4e1a0ed303946760030a5
Trudelutten <t.b.hjelmeland@gmail.com>
2019-03-01 10:45:15 +0100

    added deleteController and html code

---
+++ SqlWebApp/src/main/java/no/noroff/SqlWebApp/DeleteInput.java
@@ -0,0 +1,12 @@
+package no.noroff.SqlWebApp;
+
+public class DeleteInput {
+    String value;
+    public void setValue(String v) {
+        value = v;
+    }
+    public String getValue(){
+        return value;
+    }
+
+}
